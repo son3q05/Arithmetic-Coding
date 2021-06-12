3a31107f7e0fd3abe36859066c0b714400f110f2
son3q05 <71742331+son3q05@users.noreply.github.com>
2021-06-13 02:56:22 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,53 @@
+//Drive program for Arithmetic Coding
+//By Nguyen Thai Son - 931924
+
+#include<iostream>
+#include "ArithmeticWithScaling.cpp"
+
+using namespace std;
+
+int main(){
+    cout<<"Arithmetic Codeing - By Nguyen Thai Son"<<endl<<endl;
+
+    string input,output;
+
+    char check;
+	cout<<"Encode Phase!"<<endl;
+	
+	cout<<"Do you want to Encode?<y/n> ";
+	cin>>check;
+	if(!(check=='Y'||check=='y')) goto DECODE_PHASE;
+    cout<<"Encode!"<<endl;
+    cout<<"Input your Plaintext file name: ";
+    cin>>input;
+	const char* inputFile = input.c_str();
+
+    cout<<"Input your Destination File name: ";
+	cin>>output;
+	output.append(".arth");
+    const char* outputFile=output.c_str();
+
+    Arithmetic EncodeObj;
+    EncodeObj.Encode(inputFile,outputFile);
+
+    //Decode Phase
+	DECODE_PHASE:
+    cout<<"Decode!"<<endl;
+    cout<<"Do you want to Decode?<y/n> ";
+	cin>>check;
+	if(!(check=='Y'||check=='y')) return 0;
+
+    cout<<"Input your encoded filename: ";
+    cin>>input;
+	inputFile = input.c_str();
+    
+
+    cout<<"Input your Destination File name: ";
+	cin>>output;
+    outputFile=output.c_str();
+
+    Arithmetic DecodeObj;
+    DecodeObj.Decode(inputFile,outputFile);
+
+    return 0;
+}
